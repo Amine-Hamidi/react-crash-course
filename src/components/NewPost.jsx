8ba21465b4159d8b1838558e66f7667746fa6b3f
aminehamidi <hamidii.aminee@gmail.com>
2025-12-14 22:14:14 +0100

    add useState

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,8 +1,9 @@
 import classes from './NewPost.module.css';
-
+import { useState } from 'react';
 function NewPost() {
+  const [enteredBody, setEnteredBody] = useState('');  
   function handleChange(event) { 
-    console.log(event.target.value);
+    setEnteredBody(event.target.value);
   } 
   return (
     <form className={classes.form}>
@@ -10,6 +11,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={handleChange} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
