cfda66036fd71b235f08d40b4e03853d66f46a94
aminehamidi <hamidii.aminee@gmail.com>
2025-12-15 15:46:16 +0100

    Outputting List Data

--- src/components/ListPosts.jsx
+++ src/components/ListPosts.jsx
@@ -20,15 +20,26 @@ function ListPosts({ isPosting, onStopPosting }) {
     return (
         <>
           {isPosting && (
-            <Modal onClose={onStopPosting} onAddPost={addPostHandler}>  
+            <Modal onClose={onStopPosting} >  
               <NewPost 
                 onCancel={onStopPosting} 
+                onAddPost={addPostHandler}
               />
             </Modal>
           )}
-            <ul className={Classes.posts}>
-                <Post author="HAMIDI" body="check out the full course!" />
+            {posts.length>0 &&(
+              <ul className={Classes.posts}>
+                  {posts.map((post) => (
+                    <Post key={post.body} author={post.author} body={post.body} />
+                ))} 
             </ul>
+            )}
+            {posts.length===0 &&( 
+              <div style={ {textAlign:'center', color:'white'}}>
+                <h2>There are no posts yet.</h2>
+                <p>Start adding some!</p>
+              </div>
+              )}
         </>
     );
 }   
