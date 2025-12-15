01afe2f06f10b8152495a37d9e6b6e14583bfa2c
aminehamidi <hamidii.aminee@gmail.com>
2025-12-15 14:59:10 +0100

    added Submit

--- src/components/ListPosts.jsx
+++ src/components/ListPosts.jsx
@@ -3,35 +3,22 @@ import Classes from './ListPosts.module.css';
 import Modal from './Modal';
 import NewPost from './NewPost';
 import Post from './post';
-import { useState } from "react";
+
 
 
 
 function ListPosts({ isPosting, onStopPosting }) {
-    const [enteredBody, setEnteredBody] = useState('');  
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-    
-      
-      function handleChange(event) { 
-        setEnteredBody(event.target.value);
-      }
-      
-      function handleChangeAuthor(event) { 
-        setEnteredAuthor(event.target.value);
-      }
+   
     return (
         <>
           {isPosting && (
             <Modal onClose={onStopPosting} >  
               <NewPost 
-                onChangeBody={handleChange} 
-                onChangeAuthor={handleChangeAuthor}
                 onCancel={onStopPosting} 
               />
             </Modal>
           )}
             <ul className={Classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="HAMIDI" body="check out the full course!" />
             </ul>
         </>
--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,17 +1,38 @@
 import classes from './NewPost.module.css';
+import { useState } from 'react';
 
-function NewPost({onCancel, onChangeBody, onChangeAuthor}) {
-  
+function NewPost({onCancel}) {
+   const [enteredBody, setEnteredBody] = useState('');  
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+    
+      
+      function handleChangeBody(event) { 
+        setEnteredBody(event.target.value);
+      }
+      
+      function handleChangeAuthor(event) { 
+        setEnteredAuthor(event.target.value);
+      }
+      function handleSubmit(event) {
+        event.preventDefault();
+        const postData = {
+          body: enteredBody,
+          author: enteredAuthor
+        };
+        console.log(postData);
+        onCancel();
+      }
+      
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onChangeBody} />
+        <textarea id="body" required rows={3} onChange={handleChangeBody} />
       </p>
      
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onChangeAuthor} />
+        <input type="text" id="name" required onChange={handleChangeAuthor} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>Cancel</button>
